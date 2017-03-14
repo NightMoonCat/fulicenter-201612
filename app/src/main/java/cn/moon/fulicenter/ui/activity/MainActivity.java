db283026bf8bf3b2f06273ceca2a5c22d4018575
NightMoonCat <495982617@qq.com>
2017-03-14 17:04:35 +0800

    添加ButterKnife依赖，使用style优化布局

--- app/src/main/java/cn/moon/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/cn/moon/fulicenter/ui/activity/MainActivity.java
@@ -1,15 +1,21 @@
 package cn.moon.fulicenter.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import cn.moon.fulicenter.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCheckedChange(View view) {
+
     }
 }
