2f4665f1a0955bf44e6c36b7a9a23b5e7239abe0
NightMoonCat <495982617@qq.com>
2017-03-14 17:09:36 +0800

    使用ButterKnife实例化布局中的控件

--- app/src/main/java/cn/moon/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/cn/moon/fulicenter/ui/activity/MainActivity.java
@@ -3,19 +3,49 @@ package cn.moon.fulicenter.ui.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import cn.moon.fulicenter.R;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    @BindView(R.id.btnNewGoods)
+    RadioButton mBtnNewGoods;
+    @BindView(R.id.btnBoutique)
+    RadioButton mBtnBoutique;
+    @BindView(R.id.btnCategory)
+    RadioButton mBtnCategory;
+    @BindView(R.id.btnCollect)
+    RadioButton mBtnCollect;
+    @BindView(R.id.btnMe)
+    RadioButton mBtnMe;
+    @BindView(R.id.layout_menu)
+    RadioGroup mLayoutMenu;
+    @BindView(R.id.layout_content)
+    FrameLayout mLayoutContent;
+    Unbinder bind;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bind = ButterKnife.bind(this);
     }
 
     public void onCheckedChange(View view) {
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (bind != null) {
+            bind.unbind();
+        }
     }
 }
