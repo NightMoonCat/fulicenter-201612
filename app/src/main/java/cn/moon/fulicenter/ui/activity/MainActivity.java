66f4f58b29a4157f69dbb4e4a830bee14e685592
NightMoonCat <495982617@qq.com>
2017-03-15 20:22:17 +0800

    将两个Fragment的界面和数据以及切换显示出来

--- app/src/main/java/cn/moon/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/cn/moon/fulicenter/ui/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package cn.moon.fulicenter.ui.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -11,6 +12,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import cn.moon.fulicenter.R;
+import cn.moon.fulicenter.ui.fragment.BoutiqueFragment;
 import cn.moon.fulicenter.ui.fragment.NewGoodsFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,18 +33,55 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.layout_content)
     FrameLayout mLayoutContent;
     Unbinder bind;
+    int index = 0;
+    int currentIndex = 0;
+    Fragment[] mFragments;
+    NewGoodsFragment mNewGoodsFragment;
+    BoutiqueFragment mBoutiqueFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bind = ButterKnife.bind(this);
+        initFragment();
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.layout_content, new NewGoodsFragment())
+                .add(R.id.layout_content,mNewGoodsFragment)
+                .add(R.id.layout_content,mBoutiqueFragment)
+                .hide(mBoutiqueFragment)
+                .show(mNewGoodsFragment)
                 .commit();
+
+    }
+
+    private void initFragment() {
+        mFragments = new Fragment[5];
+        mNewGoodsFragment = new NewGoodsFragment();
+        mBoutiqueFragment = new BoutiqueFragment();
+        mFragments[0] = mNewGoodsFragment;
+        mFragments[1] = mBoutiqueFragment;
+
     }
 
     public void onCheckedChange(View view) {
+        switch (view.getId()) {
+            case R.id.btnNewGoods:
+                index = 0;
+                break;
+            case R.id.btnBoutique:
+                index = 1;
+                break;
+        }
+        setFragment();
+    }
 
+    private void setFragment() {
+        if (currentIndex != index) {
+            getSupportFragmentManager().beginTransaction()
+                    .hide(mFragments[currentIndex])
+                    .show(mFragments[index])
+                    .commit();
+            currentIndex = index;
+        }
     }
 
     @Override
