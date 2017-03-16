bf6196a085a1c744a34aef0714721dfa16da7e2a
NightMoonCat <495982617@qq.com>
2017-03-16 20:33:57 +0800

    优化GoodsDetails商品详情的数据的显示，加上其他介绍

--- app/src/main/java/cn/moon/fulicenter/ui/activity/GoodsDetailsActivity.java
+++ app/src/main/java/cn/moon/fulicenter/ui/activity/GoodsDetailsActivity.java
@@ -2,30 +2,44 @@ package cn.moon.fulicenter.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.moon.fulicenter.R;
 import cn.moon.fulicenter.application.I;
+import cn.moon.fulicenter.model.bean.AlbumsBean;
 import cn.moon.fulicenter.model.bean.GoodsDetailsBean;
 import cn.moon.fulicenter.model.net.GoodsDetailsModel;
 import cn.moon.fulicenter.model.net.IGoodsDetailsModel;
 import cn.moon.fulicenter.model.net.OnCompleteListener;
+import cn.moon.fulicenter.ui.view.FlowIndicator;
 import cn.moon.fulicenter.ui.view.MFGT;
+import cn.moon.fulicenter.ui.view.SlideAutoLoopView;
 
 public class GoodsDetailsActivity extends AppCompatActivity {
 
     int goodId;
     IGoodsDetailsModel mModel;
-    @BindView(R.id.tvGoodsEnglishName)
-    TextView mTvGoodsEnglishName;
-    @BindView(R.id.tvCurrentPrice)
-    TextView mTvCurrentPrice;
-    @BindView(R.id.tvGoodsChineseName)
-    TextView mTvGoodsChineseName;
-    @BindView(R.id.tvSalePrice)
-    TextView mTvSalePrice;
+    @BindView(R.id.tv_good_name_english)
+    TextView mTvGoodNameEnglish;
+    @BindView(R.id.tv_good_name)
+    TextView mTvGoodName;
+    @BindView(R.id.tv_good_price_shop)
+    TextView mTvGoodPriceShop;
+    @BindView(R.id.tv_good_price_current)
+    TextView mTvGoodPriceCurrent;
+    @BindView(R.id.salv)
+    SlideAutoLoopView mSalv;
+    @BindView(R.id.indicator)
+    FlowIndicator mIndicator;
+    @BindView(R.id.wv_good_brief)
+    WebView mWvGoodBrief;
+
+    GoodsDetailsBean mBean;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +59,10 @@ public class GoodsDetailsActivity extends AppCompatActivity {
     private void initData() {
         mModel.loadData(GoodsDetailsActivity.this, goodId, new OnCompleteListener<GoodsDetailsBean>() {
             @Override
-            public void onSuccess(GoodsDetailsBean result) {
-                if (result != null) {
-                    mTvGoodsEnglishName.setText(result.getGoodsEnglishName());
-                    mTvGoodsChineseName.setText(result.getGoodsName());
-                    mTvCurrentPrice.setText(result.getCurrencyPrice());
-                    mTvSalePrice.setText(result.getShopPrice());
+            public void onSuccess(GoodsDetailsBean bean) {
+                if (bean != null) {
+                    mBean = bean;
+                    showDetails(bean);
                 }
             }
 
@@ -59,6 +71,42 @@ public class GoodsDetailsActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void showDetails(GoodsDetailsBean bean) {
+        mTvGoodNameEnglish.setText(bean.getGoodsEnglishName());
+        mTvGoodName.setText(bean.getGoodsName());
+        mTvGoodPriceCurrent.setText(bean.getCurrencyPrice());
+        mTvGoodPriceShop.setText(bean.getShopPrice());
+        mSalv.startPlayLoop(mIndicator, getAlbumUrl(), getAlbumCount());
+        mWvGoodBrief.loadDataWithBaseURL(null, bean.getGoodsBrief(), I.TEXT_HTML, I.UTF_8, null);
+
+    }
+
+    private String[] getAlbumUrl() {
+        if (mBean.getPromotePrice() != null && mBean.getPromotePrice().length() > 0) {
+            AlbumsBean[] albums = mBean.getProperties()[0].getAlbums();
+            if (albums != null && albums.length > 0) {
+                String[] urls = new String[albums.length];
+                for (int i=0;i<albums.length;i++) {
+                    urls[i] = albums[0].getImgUrl();
+                    return urls;
+                }
+            }
+        }
+        return null;
+    }
+
+    private int getAlbumCount() {
+        if (mBean.getPromotePrice() != null && mBean.getPromotePrice().length() > 0) {
+            return mBean.getProperties()[0].getAlbums().length;
+        }
+        return 0;
+    }
+
+    @OnClick(R.id.ivBack)
+    public void clickBack() {
+        MFGT.finish(GoodsDetailsActivity.this);
     }
 }
 
